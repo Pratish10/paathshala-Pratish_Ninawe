862cb19947273077c84c7061d1d421a6ce70aa32
Pratish Ninawe <pratish.1006@gmail.com>
2023-07-12 17:26:17 +0530

    final commit

--- src/Pages/employeeList.js
+++ src/Pages/employeeList.js
@@ -94,13 +94,13 @@ const Wrapper = styled.div`
     .card{
         background: #fff;
         margin: 1em;
-        overflow: hidden;
+        overflow: auto;
         box-shadow: 0 13px 27px -5px hsla(240, 30.1%, 28%, 0.25), 0 8px 16px -8px hsla(0, 0%, 0%, 0.3), 0 -6px 16px -6px hsla(0, 0%, 0%, 0.03);
         transition: all ease 200ms;
         border: 1px solid black;
         padding: 20px;
         text-align: center;
-        height: 250px;
+        height: 200px;
     }
     .card:hover {
         transform: scale(1.03);
